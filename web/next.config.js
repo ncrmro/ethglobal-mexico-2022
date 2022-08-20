201b061baa5bc8ca8e1b68c02100cc4c684effc2
Nicholas Romero <ncrmro@gmail.com>
2022-08-20 16:55:39 -0500

    chore:

--- web/next.config.js
+++ web/next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  GRAPHQL_URL: "https://api-mumbai.lens.dev/",
+  publicRuntimeConfig: {
+    GRAPHQL_URL: "https://api-mumbai.lens.dev/",
+  },
 };
 
 module.exports = nextConfig;
