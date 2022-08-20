528f9e0e816927632abf4dc9273dc25fc7386186
Juan Palomino <jpalomino09@gmail.com>
2022-08-20 00:05:40 -0500

    working metamask

---
+++ web/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+import { ToastContainer } from "react-toastify";
+import { MetaMask } from "../context/MetaMask";
+
+export const NavBar = () => {
+  const { asPath: path } = useRouter();
+
+  return (
+    <>
+      <ToastContainer />
+      <header
+        className="flex justify-around items-center py-3
+				border-b-2 border-gray-100 dark:border-gray-700"
+      >
+        <nav>
+          <ul className="flex justify-around text-center">
+            <li className={path === "/" ? "active-nav" : "nav-item"}>
+              <Link href="/">Home</Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="flex items-center">
+          <MetaMask />
+        </div>
+      </header>
+    </>
+  );
+};
