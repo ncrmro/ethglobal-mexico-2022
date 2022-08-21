61d47904406f179784810835bb8befce91d4b115
Nicholas Romero <ncrmro@gmail.com>
2022-08-20 22:40:52 -0500

    chore(web): commit

--- web/routes/Posts/fetchPosts.ts
+++ web/routes/Posts/fetchPosts.ts
@@ -1,23 +1,10 @@
-import {
-  accounts,
-  comments,
-  CommentType,
-  proposal as mockedData,
-  Proposal as ProposalType,
-  User,
-} from "../../utils/mocks";
+import { post, Proposal as ProposalType } from "../../utils/mocks";
 import { PostApiRes } from "../Post/fetchPost";
 
 export interface PostsApiRes extends Omit<ProposalType, "authorAddress"> {
   posts: PostApiRes[];
 }
 
-export default function fetchProposal() {
-  const { authorAddress, ...mockedProposal } = mockedData;
-  const post: PostsApiRes = {
-    ...mockedProposal,
-    author: accounts.find((a) => a.address === authorAddress)!,
-    comments,
-  };
-  return post;
+export default function fetchPosts() {
+  return { posts: [post] };
 }
