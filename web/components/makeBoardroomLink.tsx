7a96bcd6a32b9efad25a2a4149bcfd778f5403eb
Nicholas Romero <ncrmro@gmail.com>
2022-08-21 08:14:57 -0500

    chore:

--- web/components/makeBoardroomLink.tsx
+++ web/components/makeBoardroomLink.tsx
@@ -2,33 +2,31 @@ import Link from "next/link";
 import { User } from "../utils/mocks";
 import { useViewer } from "../context/Viewer";
 import { getUserApi } from "../routes/Users/getUserApi";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const MakeBoardroomLink = (props: User) => {
-  const viewer = useViewer();
   const { address, username } = props;
   const [isVerified, setVerified] = useState(false);
 
   const verifier = async () => {
-    const user = await getUserApi(address);
-    const res = JSON.parse(user.response).worldcoin_verified;
-    setVerified(res);
-    return res;
+    try {
+      const user = await getUserApi(address);
+      if (user) {
+        const res = JSON.parse(user.response).worldcoin_verified;
+        setVerified(res);
+      }
+    } catch (e) {
+      console.error(e);
+    }
   };
-  verifier();
+  useEffect(() => {
+    verifier();
+  }, []);
+
   return (
-    <>
-      <Link passHref={true} href={`https://boardroom.io/voter/${address}`}>
-        <a target="_blank">
-          <button>
-            <p>
-              {isVerified && "🌎🪙 Verified User:  "}
-              {username}
-            </p>
-          </button>
-        </a>
-      </Link>
-    </>
+    <a href={`https://boardroom.io/voter/${address}`} target="_blank">
+      {username} {isVerified && <>🌎🪙</>}
+    </a>
   );
 };
 
